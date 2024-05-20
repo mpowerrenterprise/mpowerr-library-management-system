e0f30d2e91da3f1bed2d7f811beea15224627257
nithushan mohan <nithushan014@gmail.com>
2024-05-20 18:14:00 +0530

    docs: add tables in dashboard

--- source/dashboard.php
+++ source/dashboard.php
@@ -131,7 +131,59 @@
         <div class="page-wrapper">
             <div class="container-fluid">
              
+            <h1 class="text-center">Students Management</h1>
+                    <hr>
 
+                    <form style="margin-left: 150px; margin-right: 150px;" action="php_controllers/register.php" method="post">
+                        <div class="form-group">
+                            <label for="nicInput">NIC No</label>
+                            <input name="nic" type="text" class="form-control" id="nicInput" placeholder="Enter NIC No">
+                        </div>
+                        <div class="form-group">
+                            <label for="studentNameInput">Student Name</label>
+                            <input name="student_name" type="text" class="form-control" id="studentNameInput" placeholder="Enter Student Name">
+                        </div>
+                        <div class="form-group">
+                            <label for="gradeInput">Grade</label>
+                            <input name="grade" type="text" class="form-control" id="gradeInput" placeholder="Enter Grade">
+                        </div>
+                        <div class="form-group">
+                            <label for="emailInput">E-mail</label>
+                            <input name="email" type="email" class="form-control" id="emailInput" placeholder="Enter E-mail Address">
+                        </div>
+                        <div class="form-group">
+                            <label for="mobileInput">Mobile No</label>
+                            <input name="mobile" type="text" class="form-control" id="mobileInput" placeholder="Enter Mobile No">
+                        </div>
+                        <div class="form-group">
+                            <label for="genderInput">Gender</label>
+                            <input name="gender" type="text" class="form-control" id="genderInput" placeholder="Enter Gender">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+
+                    <h1 class="text-center">Student Management</h1>
+                    <hr>
+
+                    <table class="table table-dark">
+                        <thead>
+                            <tr>
+                                <th scope="col">NIC No</th>
+                                <th scope="col">Student Name</th>
+                                <th scope="col">Grade</th>
+                                <th scope="col">Mobile No</th>
+                                <th scope="col">E-mail</th>
+                                <th scope="col">Gender</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- Student records will go here -->
+                        </tbody>
+                    </table>
+
+                    <a href="php_controllers/logout.php" class="btn btn-secondary">Logout</a>
+                </div>
                 
             
             </div>
