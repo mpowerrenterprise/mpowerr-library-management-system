19def9779d4fef11b9ad30c4e30a9a586dd17bd4
nithushan mohan <nithushan014@gmail.com>
2024-05-20 18:09:52 +0530

    feat: add dashboard template

--- source/dashboard.php
+++ source/dashboard.php
@@ -1,130 +1,182 @@
-<?php
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db_name = "mpowerr_lms_db";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $db_name);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Fetch student details
-$sql = "SELECT * FROM student_details";
-$result = $conn->query($sql);
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html dir="ltr" lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Students Management</title>
-    <link rel="stylesheet" href="source/dash.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon.png">
+    <title>Mpowerr Library Management System</title>
+    <!-- Custom CSS -->
+    <link href="assets/libs/fullcalendar/dist/fullcalendar.min.css" rel="stylesheet" />
+    <link href="assets/extra-libs/calendar/calendar.css" rel="stylesheet" />
+    <link href="dist/css/style.min.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
-<style>
-    body{
-        background-color:wheat;
-        border: solid;
-    }
 
-input{
-    height: 50px;
-    width: 40px;
-}
-.form-group{
-    color:black;
-    margin-right: 50%;
-}
-.form-control{
-    height: 35px;
-    width: 270px;
-}
-.btn{
-    margin-right: 50%;
-    background-color: blue;
-}
-.dark{
-    width: 800px;
-    background-color: black;
-    color:white;
-}
-</style>
 <body>
-    <h1 class="text-center">Students Management</h1>
-    <hr>
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
+    <div class="preloader">
+        <div class="lds-ripple">
+            <div class="lds-pos"></div>
+            <div class="lds-pos"></div>
+        </div>
+    </div>
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <div id="main-wrapper">
+        <!-- ============================================================== -->
+        <!-- Topbar header - style you can find in pages.scss -->
+        <!-- ============================================================== -->
+        <header class="topbar" data-navbarbg="skin5">
+            <nav class="navbar top-navbar navbar-expand-md navbar-dark">
+                <div class="navbar-header" data-logobg="skin5">
+                    <!-- This is for the sidebar toggle which is visible on mobile only -->
+                    <a class="nav-toggler waves-effect waves-light d-block d-md-none" href="javascript:void(0)">
+                        <i class="ti-menu ti-close"></i>
+                    </a>
+                    <h5 style="color:white;">Mpowerr Library Management System</h5>
 
-    <form style="margin-left: 150px; margin-right: 150px;" action="php_controllers/register.php" method="post">
-        <div class="form-group">
-            <label for="nic-noInput">NIC No</label>
-            <input name="name" type="text" class="form-control" id="nameInput" placeholder=" Ender NIC No">
-        </div>
-        <div class="form-group">
-            <label for="student-nameInput">Student Name</label>
-            <input name="age" type="text" class="form-control" id="ageInput" placeholder="Enter Student Name">
-        </div>
-        </div>
-        <div class="form-group">
-            <label for="ageInput">Grade</label>
-            <input name="age" type="text" class="form-control" id="ageInput" placeholder="Enter Grade">
-        </div>
-        </div>
-        <div class="form-group">
-            <label for="ageInput">E-mail</label>
-            <input name="age" type="text" class="form-control" id="ageInput" placeholder="Enter E-mail Address">
-        </div>
-        </div>
-        <div class="form-group">
-            <label for="ageInput">Mobile N</label>
-            <input name="age" type="text" class="form-control" id="ageInput" placeholder="Enter Mobile No">
-        </div>
-        <div class="form-group">
-            <label for="marksInput">Gender</label>
-            <input name="marks" type="text" class="form-control" id="marksInput" placeholder="Enter the Gender">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+                    <a class="topbartoggler d-block d-md-none waves-effect waves-light" href="javascript:void(0)" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="ti-more"></i>
+                    </a>
+                </div>
+                <!-- ============================================================== -->
+                <!-- End Logo -->
+                <!-- ============================================================== -->
+                <div class="navbar-collapse collapse" id="navbarSupportedContent" data-navbarbg="skin5">
+                    <!-- ============================================================== -->
+                    <!-- toggle and nav items -->
+                    <!-- ============================================================== -->
+                    <ul class="navbar-nav float-left mr-auto">
+                        <li class="nav-item d-none d-md-block">
+                            <a class="nav-link sidebartoggler waves-effect waves-light" href="javascript:void(0)" data-sidebartype="mini-sidebar">
+                                <i class="mdi mdi-menu font-24"></i>
+                            </a>
+                        </li>
+                    </ul>
+                    <!-- ============================================================== -->
+                    <!-- Right side toggle and nav items -->
+                    <!-- ============================================================== -->
+                </div>
+            </nav>
+        </header>
+        <!-- ============================================================== -->
+        <!-- End Topbar header -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Left Sidebar - style you can find in sidebar.scss  -->
+        <!-- ============================================================== -->
+        <aside class="left-sidebar" data-sidebarbg="skin5">
+            <!-- Sidebar scroll-->
+            <div class="scroll-sidebar">
+                <!-- Sidebar navigation-->
+                <nav class="sidebar-nav">
+                    <ul id="sidebarnav" class="p-t-30">
+                        <li class="sidebar-item">
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link" href="index.html" aria-expanded="false">
+                                <i class="mdi mdi-view-dashboard"></i>
+                                <span class="hide-menu">Student Management</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link" href="charts.html" aria-expanded="false">
+                                <i class="mdi mdi-chart-bar"></i>
+                                <span class="hide-menu">Books Management</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link" href="widgets.html" aria-expanded="false">
+                                <i class="mdi mdi-chart-bubble"></i>
+                                <span class="hide-menu">Borrows</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link" href="tables.html" aria-expanded="false">
+                                <i class="mdi mdi-border-inside"></i>
+                                <span class="hide-menu">History</span>
+                            </a>
+                        </li>
+                        <br><br><br><br><br>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link" href="grid.html" aria-expanded="false">
+                                <i class="mdi mdi-blur-linear"></i>
+                                <span class="hide-menu">Logout</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- End Sidebar navigation -->
+            </div>
+            <!-- End Sidebar scroll-->
+        </aside>
+        <!-- ============================================================== -->
+        <!-- End Left Sidebar - style you can find in sidebar.scss  -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Page wrapper  -->
+        <!-- ============================================================== -->
+        <div class="page-wrapper">
+            <div class="container-fluid">
+             
 
-    <h1 class="text-center">Student Management</h1>
-    <hr>
-
-    <table class="dark">
-        <thead>
-            <tr>
-                <th scope="col">NIC No</th>
-                <th scope="col">Student Name</th>
-                <th scope="col">Grade</th>
-                <th scope="col">Mobile No</th>
-                <th scope="col">E-mail</th>
-                <th scope="col">Gender</th>
-                <th scope="col">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            if ($result->num_rows > 0) {
-                // Output data of each row
-                while ($row = $result->fetch_assoc()) {
-                    echo "<tr>";
-                    echo "<td>" . $row['nic_no'] . "</td>";
-                    echo "<td>" . $row['student_name'] . "</td>";
-                    echo "<td>" . $row['grade'] . "</td>";
-                    echo "<td>" . $row['mobile_no'] . "</td>";
-                    echo "<td>" . $row['email'] . "</td>";
-                    echo "<td>" . $row['gender'] . "</td>";
-                    echo "<td><a href='deletedata.php?auto_id=" . $row['auto_id'] . "' class='btn btn-danger'>Delete</a> | <a href='editdata.php?auto_id=" . $row['auto_id'] . "' class='btn btn-success'>Edit</a></td>";
-                    echo "</tr>";
-                }
-            }
-            ?>
-        </tbody>
-    </table>
-
-    <a href="php_controllers/logout.php" style="margin: 10px;" class="btn">Logout</a>
+                
+            
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Container fluid  -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- footer -->
+            <!-- ============================================================== -->
+            <footer class="footer text-center">
+                All Rights Reserved by Mpowerr Library Management System. <a href="https://wrappixel.com">Mpowerr Library Management System.</a>.
+            </footer>
+            <!-- ============================================================== -->
+            <!-- End footer -->
+            <!-- ============================================================== -->
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Page wrapper  -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="assets/libs/jquery/dist/jquery.min.js"></script>
+    <script src="dist/js/jquery.ui.touch-punch-improved.js"></script>
+    <script src="dist/js/jquery-ui.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="assets/libs/popper.js/dist/umd/popper.min.js"></script>
+    <script src="assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
+    <script src="assets/extra-libs/sparkline/sparkline.js"></script>
+    <!--Wave Effects -->
+    <script src="dist/js/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="dist/js/sidebarmenu.js"></script>
+    <!--Custom JavaScript -->
+    <script src="dist/js/custom.min.js"></script>
+    <!-- this page js -->
+    <script src="assets/libs/moment/min/moment.min.js"></script>
+    <script src="assets/libs/fullcalendar/dist/fullcalendar.min.js"></script>
+    <script src="dist/js/pages/calendar/cal-init.js"></script>
 </body>
+
 </html>
