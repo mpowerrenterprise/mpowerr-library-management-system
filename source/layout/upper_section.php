6c26d2354c5e8b6b55f90b7cc097a06e353a9a44
nithushan mohan <nithushan014@gmail.com>
2024-05-21 17:22:02 +0530

    docs: fix some errors

--- source/layout/upper_section.php
+++ source/layout/upper_section.php
@@ -86,7 +86,7 @@
                 <nav class="sidebar-nav">
                     <ul id="sidebarnav" class="p-t-30">
                         <li class="sidebar-item">
-                            <a class="sidebar-link waves-effect waves-dark sidebar-link" href="./student.php" aria-expanded="false">
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link" href="./student-management.php" aria-expanded="false">
                                 <i class="mdi mdi-view-dashboard"></i>
                                 <span class="hide-menu">Student Management</span>
                             </a>
