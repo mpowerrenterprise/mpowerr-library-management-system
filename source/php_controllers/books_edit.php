f0b2c2ecd37edf9cd4b75f08a3ea2e3f2914e181
nithushan mohan <nithushan014@gmail.com>
2024-05-23 12:29:10 +0530

    docs: update some codes

--- source/php_controllers/books_edit.php
+++ source/php_controllers/books_edit.php
@@ -2,15 +2,12 @@
 
 session_start();
 
-
 if ($_SESSION["permission"] != 'true'){
     // Redirect to dashboard.php
     header("Location: index.php");
    die();
 
 }
-
-
     $servername = "localhost";
     $username = "root";
     $password = "";
@@ -26,34 +23,31 @@ if ($_SESSION["permission"] != 'true'){
 
     $auto_id = $_GET['auto_id'];
 
-
-    $sql = "SELECT * from student_details where auto_id = '$auto_id'";
+    $sql = "SELECT * from books_details where auto_id = '$auto_id'";
     $result = $conn->query($sql);
 
-
     $book_name_from_db = "";
-    $isbn_from_db = "";
+    $isbn_no_from_db = "";
     $author_from_db = "";
-    $name_from_db = "";
-    $age_from_db = "";
-    $marks_from_db = "";
+    $price_from_db = "";
+    $release_date_from_db = "";
+    $genres_from_db = "";
 
     if ($result->num_rows > 0) {
         // output data of each row
         while($row = $result->fetch_assoc()) {
 
-            $name_from_db = $row['name'];
-            $age_from_db =  $row['age'];
-            $marks_from_db  = $row['marks'];
-        
-            
+            $book_name_from_db = $row['book_name'];
+            $isbn_no_from_db =  $row['isbn_no'];
+            $author_from_db  = $row['author'];
+            $price_from_db = $row['price'];
+            $release_date_from_db =  $row['release_date'];
+            $genres_from_db  = $row['genres'];
+                   
         } 
     } 
 
-   
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -66,23 +60,35 @@ if ($_SESSION["permission"] != 'true'){
 </head>
 <body>
 
-    <h1 style="text-align:center;">Edit Students</h1>
+    <h1 style="text-align:center;">Edit Book Details</h1>
     <hr>
-
 
     <form style="margin-left: 150px; margin-right: 150px;" ACTION="/class-system/editdata2.php" METHOD = "post">
         <div class="form-group">
-            <label for="exampleInputEmail1">Name</label>
-            <input name="name" type="text" value="<?php echo $name_from_db?>" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Name">
+            <label for="exampleInputEmail1">Book Name</label>
+            <input name="book_name" type="text" value="<?php echo $book_name_from_db?>" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Book Name">
             <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
         <div class="form-group">
-            <label for="exampleInputPassword1">Age</label>
-            <input name="age" type="text" value="<?php echo $age_from_db?>" class="form-control" id="exampleInputPassword1" placeholder="Enter Age">
+            <label for="exampleInputPassword1">ISBN No</label>
+            <input name="isbn_no" type="text" value="<?php echo $isbn_no_from_db?>" class="form-control" id="exampleInputPassword1" placeholder="Enter Age">
         </div>
         <div class="form-group">
-            <label for="exampleInputPassword1">Marks</label>
-            <input name="marks" type="text" value="<?php echo $marks_from_db?>" class="form-control" id="exampleInputPassword1" placeholder="Enter Marks">
+            <label for="exampleInputPassword1">Author</label>
+            <input name="marks" type="text" value="<?php echo $author_from_db?>" class="form-control" id="exampleInputPassword1" placeholder="Enter Author">
+        </div>
+
+        <div class="form-group">
+            <label for="exampleInputEmail1">Price</label>
+            <input name="price" type="text" value="<?php echo $price_from_db?>" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Book Price">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Age</label>
+            <input name="release_date" type="text" value="<?php echo $release_date_from_db?>" class="form-control" id="exampleInputPassword1" placeholder="Enter Age">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Genres</label>
+            <input name="genres" type="text" value="<?php echo $genres_from_db?>" class="form-control" id="exampleInputPassword1" placeholder="Enter Marks">
         </div>
 
         <input style="display:none;" name="auto_id" type="text" value="<?php echo $auto_id?>" class="form-control" id="exampleInputPassword1" placeholder="Enter Marks">
